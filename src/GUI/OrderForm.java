587e419ce2b335ac5c56e939d376123b0f3005b6
_ReD_ <apple.tyomka@gmail.com>
2017-06-12 00:26:23 +0300

    Lab1 v2.5

---
+++ src/GUI/OrderForm.java
@@ -0,0 +1,47 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Map;
+
+/**
+ * Created by _red_ on 11.06.17.
+ */
+public class OrderForm extends JFrame {
+    public final JTextArea textArea;
+
+    public OrderForm(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(200 , 300);
+        frame.setResizable(true);
+
+        frame.setLocationRelativeTo(null);
+
+        JPanel firstPanel = new JPanel();
+
+        textArea = new JTextArea(15, 13);
+        textArea.setEnabled(false);
+        textArea.setLineWrap(true);
+
+        firstPanel.add(textArea);
+
+        frame.add(firstPanel, BorderLayout.NORTH);
+
+        JScrollPane scrollBar = new JScrollPane(textArea);
+        frame.getContentPane().add(scrollBar, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
+
+    public synchronized void setTextArea(Map<String, Integer> map) {
+        this.textArea.setText(null);
+        StringBuilder str = new StringBuilder();
+        synchronized (map) {
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                str.append("Word - ").append(entry.getKey()).append(" | Entries - ").
+                        append(entry.getValue().toString()).append("\n");
+            }
+        }
+        this.textArea.append(str.toString());
+    }
+}
